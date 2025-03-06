cc9dbd07c46c4a0fac631f9df43749e553108162
AzazeIIo <61389155+AzazeIIo@users.noreply.github.com>
2025-03-06 12:21:18 +0100

    Gyűjtemények kiválasztása, átnevezése
    Ki lehet választani az aktív gyűjteményt és át is lehet nevezni dupla kattintással.
    Elhelyeztem pár tesztelemet, hogy lássam, hogy fog majd kinézni, ha lesznek elemek a gyűjteményekben.

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
 const newBtn = document.getElementById("newBtn");
 const newCollection = document.getElementById("newCollection");
+let activeCollection;
 
 window.onload = function() {
     showCollections();
+    document.getElementById("collections").onclick = function(e) {
+        if(e.target && e.target.nodeName == "LI") {
+            if (activeCollection != undefined) {
+                activeCollection.classList.toggle("active");
+            }
+            activeCollection = e.target;
+            activeCollection.classList.toggle("active");
+        }
+    }
+    
+    document.getElementById("collections").ondblclick = function(e) {
+        if(e.target && e.target.nodeName == "LI") {
+            let original = e.target.innerHTML;
+            let input = document.createElement("input");
+            input.value = original;
+            input.onkeydown = function(ev){
+                if(ev.key === 'Enter') {
+                    e.target.innerHTML = input.value;
+                    let collections = getCollections();
+                    collections[e.target.id] = input.value;                    
+                    setCollections(collections);
+                }
+            }
+            /*input.onblur = function(){
+                e.target.innerHTML = original;
+            }*/
+            e.target.innerHTML = "";
+            e.target.appendChild(input);
+            input.focus();
+        }
+    }
 }
 
 newBtn.onclick = function() {
@@ -26,8 +58,10 @@ function showCollections() {
     const collections = getCollections();
     const collectionList = document.getElementById("collections");
     collectionList.innerHTML = "";
-    collections.forEach(collection => {
-        collectionList.insertAdjacentHTML("beforeEnd", `<li class="list-group-item">${collection}</li>`);
+    console.log(collections);
+
+    collections.forEach((collection, index) => {
+        collectionList.insertAdjacentHTML("beforeEnd", `<li id="${index}" class="list-group-item list-group-item-action">${collection}</li>`);
     });
 }
 
@@ -36,9 +70,11 @@ function getCollections() {
     if (getJSON){
         let collections = JSON.parse(getJSON);
         if (collections == undefined) {
-            collections = [];
+            return [];
         }
         return collections;
+    } else {
+        return [];
     }
 }
 
